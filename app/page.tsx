290a34a13895969cb9dca1c48577683969d542bf
tchar <trishitchar@gmail.com>
2024-01-13 23:52:53 +0530

    middle col button added but volume not working, will fix later

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ import Webcam from 'react-webcam';
 import { Separator } from '@/components/ui/separator';
 import { ModeToggle } from '@/components/theme-toggle';
 import { Button } from '@/components/ui/button';
-import { Camera, Divide, FlipHorizontal, MoonIcon, PersonStanding, SunIcon, Video, Volume2 } from 'lucide-react';
+import { Camera, FlipHorizontal, MoonIcon, PersonStanding, SunIcon, Video, Volume2 } from 'lucide-react';
 import { Rings } from 'react-loader-spinner';
 import { toast } from 'sonner';
+import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
+import { Slider } from '@radix-ui/react-slider';
+import { beep } from '@/utils/audio';
+import SocialMediaLinks from '@/components/social-media-link';
 
 type Props = {};
 
@@ -19,6 +23,11 @@ const HomePage = (props: Props) => {
   const [mirrored, setMirrored] = useState<boolean>(false);
   const [isRecording, setIsRecording] = useState<boolean>(false);
   const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
+  const [volume, setVolume] = useState(0.8);
+
+  useEffect(() => {
+    // Additional setup or side effects can go here
+  }, []);
 
   return (
     <div className='flex h-screen'>
@@ -43,9 +52,15 @@ const HomePage = (props: Props) => {
           {/* Top section */}
           <div className='flex flex-col gap-2'>
             <ModeToggle />
-            <Button variant={'outline'} size={'icon'} onClick={() => {
+            <Button
+              variant={'outline'}
+              size={'icon'}
+              onClick={() => {
                 setMirrored((prev) => !prev);
-              }}><FlipHorizontal /></Button>
+              }}
+            >
+              <FlipHorizontal />
+            </Button>
             <Separator className='my-2' />
           </div>
 
@@ -83,11 +98,30 @@ const HomePage = (props: Props) => {
           {/* Bottom section */}
           <div className='flex flex-col gap-2'>
             <Separator className='my-2' />
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant={'outline'} size={'icon'}>
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider
+                  max={1}
+                  min={0}
+                  step={0.2}
+                  defaultValue={[volume]}
+                  onValueCommit={(val) => {
+                    setVolume(val[0]);
+                    beep(val[0]);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
 
         <div className='h-full flex-1 py-4 px-2 overflow-y-scroll'>
-          {/* Content in the right division */}
+          <RenderFeatureHighlightsSection />
         </div>
       </div>
     </div>
@@ -103,14 +137,100 @@ const HomePage = (props: Props) => {
 
   function toggleAutoRecord() {
     // Implement auto-record toggle
-    if(autoRecordEnabled) {
+    if (autoRecordEnabled) {
       setAutoRecordEnabled(false);
-      toast("autorecord disabled")
+      toast('Auto-record disabled');
+    } else {
+      setAutoRecordEnabled(true);
+      toast('Auto-record enabled');
     }
-    else{
-      setAutoRecordEnabled(true)
-      toast("autorecord enabled")
-    } 
+  }
+
+  function RenderFeatureHighlightsSection() {
+    return <div className="text-xs text-muted-foreground">
+      <ul className="space-y-4">
+      <li>
+          <strong>Dark Mode/Sys Theme 🌗</strong>
+          <p>Toggle between dark mode and system theme.</p>
+          <Button className="my-2 h-6 w-6" variant="outline" size="icon">
+            <SunIcon size={14} />
+          </Button>{" "}
+          /{" "}
+          <Button className="my-2 h-6 w-6" variant="outline" size="icon">
+            <MoonIcon size={14} />
+          </Button>
+        </li>
+        <li>
+          <strong>Horizontal Flip ↔️</strong>
+          <p>Adjust horizontal orientation.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={() => {
+              setMirrored((prev) => !prev)
+            }}
+          ><FlipHorizontal size={14} /></Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Take Pictures 📸</strong>
+          <p>Capture snapshots at any moment from the video feed.</p>
+          <Button
+            className='h-6 w-6 my-2'
+            variant={'outline'} size={'icon'}
+            onClick={userPromptScreenshot}
+          >
+            <Camera size={14} />
+          </Button>
+        </li>
+        <li>
+          <strong>Manual Video Recording 📽️</strong>
+          <p>Manually record video clips as needed.</p>
+          <Button className='h-6 w-6 my-2'
+            variant={isRecording ? 'destructive' : 'outline'} size={'icon'}
+            onClick={userPromptRecord}
+          >
+            <Video size={14} />
+          </Button>
+        </li>
+        <Separator />
+        <li>
+          <strong>Enable/Disable Auto Record 🚫</strong>
+          <p>
+            Option to enable/disable automatic video recording whenever
+            required.
+          </p>
+          <Button className='h-6 w-6 my-2'
+            variant={autoRecordEnabled ? 'destructive' : 'outline'}
+            size={'icon'}
+            onClick={toggleAutoRecord}
+          >
+            {autoRecordEnabled ? <Rings color='white' height={30} /> : <PersonStanding size={14} />}
+
+          </Button>
+        </li>
+
+        <li>
+          <strong>Volume Slider 🔊</strong>
+          <p>Adjust the volume level of the notifications.</p>
+        </li>
+        <li>
+          <strong>Camera Feed Highlighting 🎨</strong>
+          <p>
+            Highlights persons in{" "}
+            <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+            <span style={{ color: "#00B612" }}>green</span>.
+          </p>
+        </li>
+        <Separator />
+        <li className="space-y-4">
+          <strong>Share your thoughts 💬 </strong>
+          <SocialMediaLinks/>
+          <br />
+          <br />
+          <br />
+        </li>
+      </ul>
+    </div>
   }
 };
 
